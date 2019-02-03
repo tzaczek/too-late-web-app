8add66da85b6f7806a73e9ccbf030369096d3c26
Tomasz <tomasz.zaczek85@gmail.com>
2019-02-03 17:47:02 +0800

    goal item basic view start

--- src/app/goals/goal-item/goal-item.component.ts
+++ src/app/goals/goal-item/goal-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Goal } from './goal';
+
 @Component({
   selector: 'app-goal-item',
   templateUrl: './goal-item.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalItemComponent implements OnInit {
 
+  goal : Goal;
+
   constructor() { }
 
   ngOnInit() {
+    this.goal = new Goal();
+    this.goal.name = "My first goal";
+    this.goal.description = "This is very important goal that needs to be completed prior to job interview"
   }
 
 }
