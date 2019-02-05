b2cab302f80af0310022ee0ad4b4eb0f97bcf9da
Tomasz <tomasz.zaczek85@gmail.com>
2019-02-05 10:03:18 +0800

    library service with mocked data

---
+++ src/app/library/library.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { LIBRARIAS } from './mock-library';
+import { Library } from './libray-item/library';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LibraryService {
+
+  constructor() { }
+
+  getLibrarias(){
+    return of(LIBRARIAS);
+  }
+
+  getGoalLibray(goalId: number | string){
+    return this.getLibrarias().pipe(
+      map((libs : Library[]) => libs.find(lib => lib.goalId === +goalId))
+    );
+  }
+}
