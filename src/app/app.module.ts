62a4a856d66187bd0632137a1b055f9127666b1b
tzaczek <tomasz.zaczek85@gmial.com>
2019-04-16 12:10:09 +0200

    note start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { GoalModule } from './goals/goal.module';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
